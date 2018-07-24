f4f70361181164e41cfcbd81f0027e6be17dd6a4
Dom Belcher <dominic.belcher@gmail.com>
2018-07-24 10:44:11 +0100

    added method to get API key from SSM

---
+++ src/get-alma-api-key.js
@@ -0,0 +1,14 @@
+const AWS = require('aws-sdk/global')
+require('aws-sdk/clients/ssm')
+
+const getAlmaApiKey = () => {
+  return new AWS.SSM({ apiVersion: '2014-11-06' })
+    .getParameter({
+      Name: process.env.ALMA_API_KEY_NAME,
+      WithDecryption: true
+    })
+    .promise()
+    .then(data => data.Value)
+}
+
+module.exports = getAlmaApiKey
